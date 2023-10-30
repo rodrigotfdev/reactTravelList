2fa94321d924955ade8d004bb76ead1954c5ba20
Rodrigo <rtf.franca@gmail.com>
2023-10-30 15:29:10 -0300

    Controlled Elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./index.css";
 
 const initialItems = [
@@ -26,21 +26,42 @@ function Logo() {
 }
 
 function Form() {
+  const [desc, setDesc] = useState("");
+  const [qty, setQty] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!desc) return;
+
+    const newItem = { desc, qty, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDesc("");
+    setQty(1);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for yout trip?</h3>
-      <select name="" id="">
+      <select
+        name=""
+        id=""
+        value={qty}
+        onChange={(e) => setQty(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={desc}
+        onChange={(e) => setDesc(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
