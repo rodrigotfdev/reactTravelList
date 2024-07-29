aa6e4a244eaff86685dcc46738806b4800cdbede
Rodrigo <rtf.franca@gmail.com>
2024-07-28 23:42:28 -0300

    feat(form): control form elements with state

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,22 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1);
   }
 
   return (
-    <div className="add-form" onSubmit={handleSubmit}>
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 10 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" name="" id="" placeholder="Item..." />
+      <input
+        type="text"
+        name=""
+        id=""
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
-    </div>
+    </form>
   );
 }
